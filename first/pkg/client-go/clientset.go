394103a511e248fa9b6a1ed9ed2d127238735c77
leon.zhao <zxfleon@163.com>
2020-02-05 01:07:51 +0800

    k8s curl

--- first/pkg/client-go/clientset.go
+++ first/pkg/client-go/clientset.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	appv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/core/v1"
+	//v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
