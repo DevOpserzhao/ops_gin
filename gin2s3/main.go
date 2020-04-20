cddfd9e95c2353254e1c536d8b73dd9c4636eb86
leon.zhao <zxfleon@163.com>
2020-04-20 09:37:17 +0800

    更新

---
+++ gin2s3/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func upload(c *gin.Context) {
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
+		return
+	}
+
+	filename := header.Filename
+	out, err := os.Create("public/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//filepath := "http://localhost:8080/file/" + filename
+	//c.JSON(http.StatusOK, gin.H{"filepath": filepath})
+	bucket := "testzxf"
+	//sess, err := session.NewSession(&aws.Config{
+	//	Region: aws.String("cn-northwest-1")},
+	//)
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("cn-northwest-1"),
+		Credentials: credentials.NewSharedCredentials(".aws/credentials", "default"),
+	})
+
+	// Setup the S3 Upload Manager. Also see the SDK doc for the Upload Manager
+	// for more information on configuring part size, and concurrency.
+	//
+	// http://docs.aws.amazon.com/sdk-for-go/api/service/s3/s3manager/#NewUploader
+	uploader := s3manager.NewUploader(sess)
+
+	// Upload the file's body to S3 bucket as an object with the key being the
+	// same as the filename.
+	s3_re := &s3manager.UploadOutput{}
+
+	s3_re, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+
+		// Can also use the `filepath` standard library package to modify the
+		// filename as need for an S3 object key. Such as turning absolute path
+		// to a relative path.
+		Key: aws.String(filename),
+		//Key: aws.String(key),
+		// The file to be uploaded. io.ReadSeeker is preferred as the Uploader
+		// will be able to optimize memory when uploading large content. io.Reader
+		// is supported, but will require buffering of the reader's bytes for
+		// each part.
+		Body:        file,
+		ContentType: aws.String("image/jpeg"),
+		//Metadata:   map[string]*string{
+		//	"Content-Type": aws.String("image/jpeg"),
+		//},
+
+	})
+	if err != nil {
+		// Print the error and exit.
+		exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
+		c.JSON(http.StatusOK, gin.H{"错误": 000000})
+	}
+
+	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+	filepath := "http://localhost:8080/file/" + filename
+	c.JSON(http.StatusOK, gin.H{"s3": s3_re,
+		"sever": filepath,
+	})
+
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("template/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "select_file.html", gin.H{})
+	})
+
+	router.POST("/upload", upload)
+
+	router.StaticFS("/file", http.Dir("public"))
+	router.Run(":8080")
+
+}
--- k8s-deploy/go.mod
+++ k8s-deploy/go.mod
@@ -1,39 +1,18 @@
 module github.com/DevOpserzhao/ops_gin/k8s-deploy
 
-go 1.13
+go 1.14
 
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
-	github.com/astaxie/beego v1.12.0
+	github.com/astaxie/beego v1.12.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6 // indirect
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-ini/ini v1.52.0
-	github.com/go-openapi/spec v0.19.6 // indirect
-	github.com/go-openapi/swag v0.19.7 // indirect
-	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/googleapis/gnostic v0.4.1 // indirect
-	github.com/imdario/mergo v0.3.8 // indirect
-	github.com/json-iterator/go v1.1.9 // indirect
-	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/mailru/easyjson v0.7.0 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/swaggo/gin-swagger v1.2.0
 	github.com/swaggo/swag v1.6.5
 	github.com/unknwon/com v1.0.1
-	golang.org/x/crypto v0.0.0-20200204104054-c9f3fb736b72 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
-	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	golang.org/x/tools v0.0.0-20200204230316-67a4523381ef // indirect
-	google.golang.org/appengine v1.6.5 // indirect
 	k8s.io/api v0.17.2
 	k8s.io/apimachinery v0.17.2
 	k8s.io/client-go v0.17.2
-	k8s.io/utils v0.0.0-20200124190032-861946025e34 // indirect
-	sigs.k8s.io/yaml v1.2.0 // indirect
 )
